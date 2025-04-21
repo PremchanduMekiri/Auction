3d32a26edbcdd590a0f93bd17733921d360cf18b
PremchanduMekiri <premmekiri22@gmail.com>
2025-04-21 11:53:32 +0530

    user regis

---
+++ auction12/src/main/java/com/example/demo/entity/RegisterRequest.java
@@ -0,0 +1,63 @@
+package com.example.demo.entity;
+
+
+
+public class RegisterRequest {
+    private String name;
+    private String email;
+    private String accounttype;
+    private String password;
+    private String confirmPassword;
+
+    public RegisterRequest() {
+    }
+
+    public RegisterRequest(String name, String email, String Accounttype, String password, String confirmPassword) {
+        this.name = name;
+        this.email = email;
+        this.accounttype= Accounttype;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+  
+    public String getAccounttype() {
+        return accounttype;
+    }
+
+    public void setAccounttype(String accounttype) {
+        accounttype = accounttype;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
---
+++ auction12/src/main/java/com/example/demo/entity/Users.java
@@ -0,0 +1,30 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "Users12", schema = "public")
+
+@Data
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+    private String password;
+    private String Accounttype;
+   
+    private String status;
+    
+
+    // Default constructor setting the status to 'pending'
+    public Users() {
+        this.status = "pending";  // Default value
+    }
+}
+   
